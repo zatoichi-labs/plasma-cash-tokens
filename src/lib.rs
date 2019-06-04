02d713699e9405f70f0afcc60b83d01a44dc0950
Bryant Eisenbach <fubuloubu@gmail.com>
2019-06-04 16:24:58 -0400

    refactor: Vanilla transactions are test-only
    - Now using a trait to verify behavior
    - Reduced dependencies and logic-only
    - Also refactored a bit of the comparision and metadata logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,121 +1,53 @@
-extern crate rand;
-
 extern crate ethereum_types;
 pub use ethereum_types::{Address, U256};
 
-extern crate secp256k1;
-use secp256k1::{Secp256k1, Message, RecoverableSignature, key};
-
-extern crate keccak_hash;
-use keccak_hash::keccak;
-
-extern crate ethabi;
-
-fn pkey_to_address(pkey: &key::PublicKey) -> Address {
-    let ctx = Secp256k1::new();
-    let pkey_hash = keccak(pkey.serialize_vec(&ctx, false));
-    Address::from_slice(&pkey_hash[..20])
+#[derive(Debug, PartialEq)]
+pub enum TxnCmp {
+    Same, // LHS == RHS
+    Parent, // LHS is the parent of RHS
+    Child, // RHS is the parent of LHS
+    EarlierSibling, // LHS & RHS have same parent, but LHS is earlier
+    LaterSibling, // LHS & RHS have same parent, but RHS is earlier
+    DoubleSpend, // LHS & RHS are the same txn to two different receivers
+    Unrelated, // LHS & RHS have no relationship to each other
 }
 
-// camelCase is used here because of EIP-712
-#[allow(non_snake_case)]
-pub struct Transaction {
-    pub newOwner: Address,
-    pub tokenId: U256,
-    pub prevBlkNum: U256,
-    signature: Option<RecoverableSignature>,
+/// Plasma Transactions form a DAG where only one pathway back is
+/// considered legitimate. However, there may be multiple pathways,
+/// so it is important to allow this behavior to be compared.
+pub trait PlasmaCashTxn {
+    /// Simply gives the receiver of the transaction, if available.
+    /// Note: This info may not be public, e.g. Zero Knowledge Proofs
+    ///       (In that scenario, only certain parties can see this.)
+    fn receiver(&self) -> Option<Address>;
+
+    /// Simply gives the sender of the transaction, if available.
+    /// Note: This info may not be public, e.g. Zero Knowledge Proofs
+    ///       (In that scenario, only certain parties can see this.)
+    fn sender(&self) -> Option<Address>;
+
+    /// Returns the relationship of another transaction (RHS) to this one (LHS)
+    fn compare(&self, other: &Self) -> TxnCmp;
 }
 
-impl Transaction {
-    // camelCase is used here because of EIP-712
-    #[allow(non_snake_case)]
-    pub fn new(newOwner: Address,
-               tokenId: U256,
-               prevBlkNum: U256) -> Transaction
-    {
-        Transaction {
-            newOwner,
-            tokenId,
-            prevBlkNum,
-            signature: None,
-        }
-    }
-
-    // camelCase is used here because of EIP-712
-    #[allow(non_snake_case)]
-    pub fn new_signed(newOwner: Address,
-                      tokenId: U256,
-                      prevBlkNum: U256,
-                      signature: RecoverableSignature) -> Transaction
-    {
-        Transaction {
-            newOwner,
-            tokenId,
-            prevBlkNum,
-            signature: Some(signature),
-        }
-    }
-
-    pub fn unsigned_message(&self) -> Message {
-        // Construct vector of Tokens
-        let new_owner = ethabi::Token::Address(self.newOwner);
-        let token_id = ethabi::Token::Uint(self.tokenId);
-        let prev_blk_num = ethabi::Token::Uint(self.prevBlkNum);
-        let msg_vec = &[new_owner, token_id, prev_blk_num];
-        // Encode vector of Tokens
-        let msg_bytes = ethabi::encode(msg_vec);
-        // Return keccak hash of encoded struct
-        Message::from_slice(keccak(msg_bytes).as_fixed_bytes()).unwrap()
-    }
-
-    pub fn signer(&self) -> Address {
-        let ctx = Secp256k1::new();
-        let msg_hash = self.unsigned_message();
-        let pkey = ctx.recover(&msg_hash, &self.signature.unwrap()).unwrap();
-        pkey_to_address(&pkey)
-    }
-}
-
-pub fn is_history_valid(history: &[Transaction]) -> bool {
+pub fn is_history_valid<T: PlasmaCashTxn>(history: &[T]) -> bool {
     // If token has no history, return True
     if history.len() == 0 {
         return true;
     }
 
-    // History is valid if each transaction was signed by the previous receipient
-    let signers: Vec<Address> = history.iter()
-                                .skip(1) // Don't care about signer of 1st txn
-                                .map(|txn| txn.signer())
-                                .collect();
-    let receivers: Vec<Address> = history.iter()
-                                  // Note: Due to skip(1) above, this iterator
-                                  // will exhaust before the last entry is used
-                                  .map(|txn| txn.newOwner)
-                                  .collect();
-    let valid_txn_history = signers.iter()
-                            .zip(receivers.iter())
-                            .all(|a| a.0 == a.1);
+    // History is valid if each transaction follows the previous
+    let mut history_iter = history.iter().peekable();
+    while let Some(prev_txn) = history_iter.next() {
+        if let Some(txn) = history_iter.peek() {
+            match prev_txn.compare(&txn) {
+                TxnCmp::Parent => { },
+                _ => { return false },
+            }
+        }
+    }
 
-    // Check that each transaction uses the same token ID (matching the one we have)
-    let uid = history.first().unwrap().tokenId;
-    let all_same_token_uid = history.iter()
-                             .map(|txn| txn.tokenId)
-                             .all(|token_id| token_id == uid);
-
-    // Check that each transition increases the value of prevBlkNum from the former
-    //let blk_ref_increases = history.iter()
-    //                        .map(|txn| txn.prevBlkNum) // Reference to Plasma block prior to txn
-    //                        .is_sorted_by(|prior_txn_blk, txn_blk| prior_txn_blk < txn_blk);
-    //                        FIXME: is_sorted_by is in nightly...
-    let mut blk_ref_increases = true;
-    let mut prior_txn_blk = history.first().unwrap().prevBlkNum;
-    history.iter().skip(1).for_each(|txn| {
-        blk_ref_increases &= prior_txn_blk < txn.prevBlkNum;
-        prior_txn_blk = txn.prevBlkNum;
-    });
-
-    // return:
-    valid_txn_history && all_same_token_uid && blk_ref_increases
+    true
 }
 
 #[derive(Debug, PartialEq)]
@@ -126,42 +58,167 @@ pub enum TokenStatus {
     Withdrawal,
 }
 
-pub struct Token {
+pub struct Token<T: PlasmaCashTxn> {
     pub uid: U256,
-    pub owner: Address,
     pub status: TokenStatus,
-    pub history: Vec<Transaction>,
+    pub history: Vec<T>,
 }
 
-impl Token {
-    pub fn new(uid: U256, owner: Address) -> Token {
+impl<T: PlasmaCashTxn> Token<T> {
+    pub fn new(uid: U256) -> Token<T> {
         Token {
             uid,
-            owner,
             status: TokenStatus::RootChain,
             history: Vec::new(),
         }
     }
     
     pub fn is_valid(&self) -> bool {
-        let current_owner = match self.history.len() {
-            0 => self.owner, //token.ownerOf(self.uid),
-            _ => self.history.last().unwrap().newOwner,
-        };
-
-        is_history_valid(&self.history) && current_owner == self.owner
+        is_history_valid(&self.history)
     }
 
-    pub fn add_transaction(&mut self, txn: Transaction) {
-        assert_eq!(txn.signer(), self.owner);
-        self.owner = txn.newOwner;
+    pub fn add_transaction(&mut self, txn: T) {
+        if self.history.len() > 0 {
+            assert_eq!(txn.compare(self.history.last().unwrap()), TxnCmp::Parent);
+        }
         self.history.push(txn);
+    }
+
+    pub fn owner(&self) -> Address {
+        // TODO Call out to Web3 instead if in the RootChain
+        self.history.last().unwrap().receiver().unwrap()
     }
 }
 
 
 #[cfg(test)]
 mod tests {
+    extern crate rand;
+
+    extern crate secp256k1;
+    use secp256k1::{Secp256k1, Message, RecoverableSignature, key};
+
+    extern crate keccak_hash;
+    use keccak_hash::keccak;
+
+    extern crate ethabi;
+
+    fn pkey_to_address(pkey: &key::PublicKey) -> Address {
+        let ctx = Secp256k1::new();
+        let pkey_hash = keccak(pkey.serialize_vec(&ctx, false));
+        Address::from_slice(&pkey_hash[..20])
+    }
+
+    // camelCase is used here because of EIP-712
+    #[allow(non_snake_case)]
+    pub struct Transaction {
+        pub newOwner: Address,
+        pub tokenId: U256,
+        pub prevBlkNum: U256,
+        signature: Option<RecoverableSignature>,
+    }
+
+    impl Transaction {
+        // camelCase is used here because of EIP-712
+        #[allow(non_snake_case)]
+        pub fn new(newOwner: Address,
+                   tokenId: U256,
+                   prevBlkNum: U256) -> Transaction
+        {
+            Transaction {
+                newOwner,
+                tokenId,
+                prevBlkNum,
+                signature: None,
+            }
+        }
+
+        // camelCase is used here because of EIP-712
+        #[allow(non_snake_case)]
+        pub fn new_signed(newOwner: Address,
+                          tokenId: U256,
+                          prevBlkNum: U256,
+                          signature: RecoverableSignature) -> Transaction
+        {
+            Transaction {
+                newOwner,
+                tokenId,
+                prevBlkNum,
+                signature: Some(signature),
+            }
+        }
+
+        pub fn unsigned_message(&self) -> Message {
+            // Construct vector of Tokens
+            let new_owner = ethabi::Token::Address(self.newOwner);
+            let token_id = ethabi::Token::Uint(self.tokenId);
+            let prev_blk_num = ethabi::Token::Uint(self.prevBlkNum);
+            let msg_vec = &[new_owner, token_id, prev_blk_num];
+            // Encode vector of Tokens
+            let msg_bytes = ethabi::encode(msg_vec);
+            // Return keccak hash of encoded struct
+            Message::from_slice(keccak(msg_bytes).as_fixed_bytes()).unwrap()
+        }
+
+        pub fn sign(&self, skey: &key::SecretKey) -> Transaction {
+            assert!(self.signature.is_none());
+            let ctx = Secp256k1::new();
+            let sig = ctx.sign_recoverable(&self.unsigned_message(), skey).unwrap();
+            Transaction::new_signed(self.newOwner, self.tokenId, self.prevBlkNum, sig)
+        }
+    }
+
+    impl PlasmaCashTxn for Transaction {
+        fn receiver(&self) -> Option<Address> {
+            Some(self.newOwner)
+        }
+
+        fn sender(&self) -> Option<Address> {
+            let ctx = Secp256k1::new();
+            let msg_hash = self.unsigned_message();
+            let pkey = ctx.recover(&msg_hash, &self.signature.unwrap()).unwrap();
+            Some(pkey_to_address(&pkey))
+        }
+
+        fn compare(&self, other: &Transaction) -> TxnCmp {
+
+            // Transactions must be with the same tokenId to be related
+            if self.tokenId == other.tokenId {
+
+                // The other one is the direct parent of this one
+                if self.newOwner == other.sender().unwrap() {
+                    return TxnCmp::Parent; // FIXME Because this comes first, a cycle is possible
+
+                // This one is the direct parent of the other one
+                } else if self.sender().unwrap() == other.newOwner {
+                    return TxnCmp::Child;
+
+                // Both of us have the same parent
+                } else if self.sender().unwrap() == other.sender().unwrap() {
+
+                    // But mine comes before, so I'm earlier
+                    if self.prevBlkNum < other.prevBlkNum {
+                        return TxnCmp::EarlierSibling;
+
+                    // The other comes before, so I'm later
+                    } else if self.prevBlkNum > other.prevBlkNum {
+                        return TxnCmp::LaterSibling;
+
+                    // We're both at the same height, but different destinations!
+                    } else if self.newOwner != other.newOwner {
+                        return TxnCmp::DoubleSpend;
+                    }
+
+                    // We're both the same transaction (same tokenId, reciever, and sender)
+                    return TxnCmp::Same;
+                }
+            }
+
+            // All else fails, we're unrelated
+            TxnCmp::Unrelated
+        }
+    }
+
     use super::*;
 
     fn gen_addr_and_skey_pair(data: &[u8]) -> (Address, key::SecretKey) {
@@ -173,21 +230,12 @@ mod tests {
     }
 
     #[test]
-    fn create_a_token() {
-        let uid = U256::from(123);
-        let (a, _) = gen_addr_and_skey_pair(&[1; 32]);
-        let t = Token::new(uid, a);
-        assert_eq!(t.uid, uid);
-        assert_eq!(t.owner, a);
-        assert_eq!(t.status, TokenStatus::RootChain);
-        assert_eq!(t.history.len(), 0);
-    }
-
-    #[test]
     fn validate_empty_token() {
         let uid = U256::from(123);
-        let (a, _) = gen_addr_and_skey_pair(&[1; 32]);
-        let t = Token::new(uid, a);
+        let t: Token<Transaction> = Token::new(uid);
+        assert_eq!(t.uid, uid);
+        assert_eq!(t.status, TokenStatus::RootChain);
+        assert_eq!(t.history.len(), 0);
         assert!(t.is_valid());
     }
 
@@ -195,11 +243,9 @@ mod tests {
     fn add_transaction() {
         let (a, skey) = gen_addr_and_skey_pair(&[1; 32]);
         let uid = U256::from(123);
-        let mut t = Token::new(uid, a);
-        let txn = Transaction::new(a, uid, U256::from(0));
-        let ctx = Secp256k1::new();
-        let sig = ctx.sign_recoverable(&txn.unsigned_message(), &skey).unwrap();
-        let txn = Transaction::new_signed(a, uid, U256::from(0), sig);
+        let mut t: Token<Transaction> = Token::new(uid);
+        let txn = Transaction::new(a, uid, U256::from(0)).sign(&skey);
+
         assert_eq!(t.history.len(), 0);
         t.add_transaction(txn);
         assert_eq!(t.history.len(), 1);
@@ -208,26 +254,22 @@ mod tests {
 
     #[test]
     fn lots_of_history() {
-        let ctx = Secp256k1::new();
         // Same token
         let uid = U256::from(123);
+
         // 3 accounts
         let (a1, skey1) = gen_addr_and_skey_pair(&[1; 32]);
         let (a2, skey2) = gen_addr_and_skey_pair(&[2; 32]);
         let (a3, skey3) = gen_addr_and_skey_pair(&[3; 32]);
-        // a3 -> a1
-        let txn1 = Transaction::new(a1, uid, U256::from(0));
-        let sig = ctx.sign_recoverable(&txn1.unsigned_message(), &skey3).unwrap();
-        let txn1 = Transaction::new_signed(a1, uid, U256::from(0), sig);
-        // a1 -> a2
-        let txn2 = Transaction::new(a2, uid, U256::from(1));
-        let sig = ctx.sign_recoverable(&txn2.unsigned_message(), &skey1).unwrap();
-        let txn2 = Transaction::new_signed(a2, uid, U256::from(1), sig);
-        // a2 -> a3
-        let txn3 = Transaction::new(a3, uid, U256::from(2));
-        let sig = ctx.sign_recoverable(&txn3.unsigned_message(), &skey2).unwrap();
-        let txn3 = Transaction::new_signed(a3, uid, U256::from(2), sig);
-        // History should all be valid!
+
+        // Construct history...
+        // txn1: a3 -> a1
+        let txn1 = Transaction::new(a1, uid, U256::from(0)).sign(&skey3);
+        // txn2: a1 -> a2
+        let txn2 = Transaction::new(a2, uid, U256::from(1)).sign(&skey1);
+        // txn3: a2 -> a3
+        let txn3 = Transaction::new(a3, uid, U256::from(2)).sign(&skey2);
+
         let txns = vec![txn1, txn2, txn3];
         assert!(is_history_valid(&txns));
     }
