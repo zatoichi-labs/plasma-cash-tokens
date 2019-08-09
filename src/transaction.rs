c4187c50dc73cb5f25fca46d6348aec1478cb2ba
Bryant Eisenbach <fubuloubu@gmail.com>
2019-08-09 11:20:20 -0400

    doc: Updated TxnCmp docs

--- src/transaction.rs
+++ src/transaction.rs
@@ -2,15 +2,30 @@ use bitvec::prelude::BitVec;
 
 use crate::merkle::get_root;
 
+/// Returns the relationship of another transaction (RHS) to this one (LHS).
+///
+/// # Note
+/// This is used in the history verification logic, as well as withdrawal
+/// challenge detection logic. Different clients may have a privledged view
+/// of this ordering, since transactions may be encrypted in some context and
+/// unencrypted in others, which means relationships may differ depending on
+/// information privledge of the client.
 #[derive(Debug, PartialEq)]
 pub enum TxnCmp {
-    Same, // LHS & RHS are the same exact transaction
-    Parent, // LHS is the parent of RHS
-    Child, // RHS is the parent of LHS
-    EarlierSibling, // LHS & RHS have same parent, but LHS is earlier
-    LaterSibling, // LHS & RHS have same parent, but RHS is earlier
-    DoubleSpend, // LHS & RHS are the same txn to two different receivers
-    Unrelated, // LHS & RHS have no relationship to each other
+    /// LHS & RHS are the same exact transaction
+    Same,
+    /// LHS is the parent of RHS
+    Parent,
+    /// RHS is the parent of LHS
+    Child,
+    /// LHS & RHS have same parent, but LHS is earlier
+    EarlierSibling,
+    /// LHS & RHS have same parent, but RHS is earlier
+    LaterSibling,
+    /// LHS & RHS are the same txn to two different receivers
+    DoubleSpend,
+    /// LHS & RHS have no relationship to each other
+    Unrelated,
 }
 
 /// Plasma Transactions form a DAG where only one pathway back is
@@ -53,14 +68,8 @@ pub trait PlasmaCashTxn<HashType>
     /// Function used to verify proofs
     fn hash_fn() -> (fn(&[u8]) -> HashType);
 
-    /// Returns the relationship of another transaction (RHS) to this
-    /// one (LHS). See TxnCmp enum definition for more information.
-    /// Note: This is used in the history verification logic, as well as
-    ///       withdrawal challenge detection logic.
-    /// Note: Different clients may have a privledged view of this ordering,
-    ///       since transactions may be encrypted in some context and un-
-    ///       encrypted in others, which means relationships may differ
-    ///       depending on information privledge of the client.
+    /// Returns the relationship of another transaction (other) to this
+    /// one (self). See TxnCmp enum definition for more information.
     fn compare(&self, other: &Self) -> TxnCmp;
 
     /// Obtain the root hash following the SMT algorithm
